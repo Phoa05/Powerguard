a0adf56e777bae79b9397856b51df37046be6576
Pedro Henrique <pedro.phoa1405@gmail.com>
2025-06-02 22:21:00 -0300

    feat: adicionando os repositorys

---
+++ src/main/java/com/fiap/powerguard/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.fiap.powerguard.repository;
+
+import com.fiap.powerguard.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    List<User> findByCity(String city);
+}
