57ef628363b84bac0012e0bd5fd4990e151f7332
Pedro Henrique <pedro.phoa1405@gmail.com>
2025-06-02 21:46:24 -0300

    feat: iniciando projeto

---
+++ src/main/java/com/fiap/powerguard/PowerguardApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.powerguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/powerguard/PowerguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.powerguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
